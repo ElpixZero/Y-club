a7d44900f271e594afcc69c36863d3b62a4a6bce
ElpixJunior <nasurov.eldar@mail.ru>
2020-02-14 18:47:08 +0300

    Finished

---
+++ build/precache-manifest.8bc47dbd75d31999c421c34783c6a68c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0513ac94ff4db90f358c175043b1131",
+    "url": "/Y-club/index.html"
+  },
+  {
+    "revision": "63cf46ae1cb266d8cc74",
+    "url": "/Y-club/static/js/2.706b85dc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Y-club/static/js/2.706b85dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0d62820f4d236dd743",
+    "url": "/Y-club/static/js/main.7311878e.chunk.js"
+  },
+  {
+    "revision": "39559f497743bf50e906",
+    "url": "/Y-club/static/js/runtime-main.5c6638ab.js"
+  }
+]);
